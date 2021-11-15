30cfe0c68545c2de1b714d7fc6c56e145adc1219
Vadim Gubaidulinn <gubaidulinvadim@gmail.com>
2021-11-15 19:35:07 +0100

    small changes+LHC PEL lens SD and tunes

--- Schenk.py
+++ Schenk.py
@@ -11,8 +11,8 @@ from matplotlib import pyplot as plt
 import seaborn as sbs
 import time
 from tune_calculation import *
-MAX_INTEGRAL_LIMIT = 16
-EPSILON = 1e-6
+# MAX_INTEGRAL_LIMIT = 16
+# EPSILON = 1e-6
 @np.vectorize
 def Q_detuning(phi: float, Jz: float, dQmax=0.001):
     return dQmax*np.exp(-0.25*Jz*(1-np.cos(2*phi)))
@@ -24,7 +24,7 @@ def Q_average_detuning(Jz: float, dQmax=0.001):
 
 
 @np.vectorize
-def B_integrand(phi: float, Jz: float, dQmax=.001):
+def B_integrand(phi: float, Jz: float, dQmax=.002):
     return Q_detuning(phi, Jz, dQmax) - Q_average_detuning(Jz, dQmax)
 
 
@@ -35,9 +35,10 @@ def B(func: object, Jz: float, phi: float):
 
 @np.vectorize
 def H_integrand(phi: float, Jz: float, p: int, l: int):
-    omega_p = p*OMEGA_REV+Q_X*OMEGA_REV+l*OMEGA_S
+    # omega_p = p*OMEGA_REV+Q_X*OMEGA_REV+l*OMEGA_S
     JzB = Jz/(SIGMA_Z**2/(2*BETA_Z))
-    return np.exp(1j*l*phi)*np.exp(-1j*omega_p/c*np.sqrt(2*Jz*BETA_Z)*np.cos(phi)) * np.exp(-1j/Q_S*B(B_integrand, JzB, phi))
+    # *np.exp(-1j*omega_p/c*np.sqrt(2*Jz*BETA_Z)*np.cos(phi))
+    return np.exp(1j*l*phi) * np.exp(-1j/Q_S*B(B_integrand, JzB, phi))
 
 
 @np.vectorize
@@ -64,33 +65,37 @@ if __name__ == '__main__':
             style='ticks',
             palette='colorblind',
             context='talk')
-    Jz = 1.0
-    assert (Q_average_detuning(Jz) - 1/(2*pi)*quad(Q_detuning, 0, 2 *
-                                                   pi, args=(Jz,))[0] < EPSILON), 'Detuning implemented incorrectly'
-    Jz = np.linspace(0, 3, 20)
+    # Jz = 1.0
+    # assert (Q_average_detuning(Jz) - 1/(2*pi)*quad(Q_detuning, 0, 2 *
+    #    pi, args=(Jz,))[0] < EPSILON), 'Detuning implemented incorrectly'
+    # Jz = np.linspace(0, 3, 20)
     # SIGMA_Z = 0.06
     # BETA_Z = 815.6
-    Jz *= SIGMA_Z**2/(2*BETA_Z)
+    # Jz *= SIGMA_Z**2/(2*BETA_Z)
     sbs.set_palette('colorblind')
     # print(max(Jz), 3*SIGMA_Z**2/(2*BETA_Z))
     # phi = np.linspace(0, 2*pi, 1000)
-    # Jz = 1
-    # plt.plot(phi, B(B_integrand, Jz, phi))
+    Jz = np.linspace(0, 90, 1000)
+    dQmax = 2e-3
+    phi = .25*pi
+    plt.plot(Jz, B(B_integrand, Jz, phi)/dQmax)
+    print()
+    print('max value of the B function:', max(B(B_integrand, Jz, phi)/dQmax))
     time_start = time.process_time()
     p = 0
     l = 0
     print('Order of magnitude for longitudinal amplitude: ',
           SIGMA_Z/(BETA*c)*OMEGA_REV)
 
-    Hr, Hi = H(Jz, p=p, l=l)
+    # Hr, Hi = H(Jz, p=p, l=l)
     time_elapsed = time.process_time()-time_start
     print('Time elapsed: {0:.2e}'.format(time_elapsed))
-    plt.plot(Jz/(SIGMA_Z**2/(2*BETA_Z)), np.sqrt(Hr**2+Hi**2), c='b',
-             linewidth=2, marker='o', markersize=2, label='$|H^0_l(J_z)|$')
+    # plt.plot(Jz/(SIGMA_Z**2/(2*BETA_Z)), np.sqrt(Hr**2+Hi**2), c='b',
+    #  linewidth=2, marker='o', markersize=2, label='$|H^0_l(J_z)|$')
 
     omega_p = p*OMEGA_REV+Q_X*OMEGA_REV+l*OMEGA_S
-    plt.plot(Jz/(SIGMA_Z**2/(2*BETA_Z)),
-             np.abs(jv(l, np.sqrt(2*Jz*BETA_Z)/c*(omega_p))), c='r', linewidth=1, marker='o', markersize=1, label='$|J_l(J_z)|$')
+    # plt.plot(Jz/(SIGMA_Z**2/(2*BETA_Z)),
+    #  np.abs(jv(l, np.sqrt(2*Jz*BETA_Z)/c*(omega_p))), c='r', linewidth=1, marker='o', markersize=1, label='$|J_l(J_z)|$')
     # plt.plot(Jz/(SIGMA_Z**2/(2*BETA_Z)), Hi,
     #  c='b', linestyle='dashed', marker='o', markersize=1, alpha=0.5)
     plt.xlabel('$J_z/\epsilon_z$')
