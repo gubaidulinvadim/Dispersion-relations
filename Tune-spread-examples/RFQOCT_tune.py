0b1be7ef40922d63dbbf7dc4bb4c14b551fd7ce5
Vadim Gubaidulinn <gubaidulinvadim@gmail.com>
2021-12-16 16:42:18 +0100

    added marginal histograms for no OCT case

--- Tune-spread-examples/RFQOCT_tune.py
+++ Tune-spread-examples/RFQOCT_tune.py
@@ -20,8 +20,8 @@ def plot_spread(dQx, dQy, normalise=True, color=None):
                        'bins': 25, }, ratio=3,
                        # xlim=(-0.1, 7.), ylim=(-.1, 7.)
                        )
-    ax.ax_joint.plot(np.mean(dQx)/dQrms_x, np.mean(dQy)/dQrms_y,
-                     marker='o', c='darkgreen', label='PEL coherent tune shift')
+    # ax.ax_joint.plot(np.mean(dQx)/dQrms_x, np.mean(dQy)/dQrms_y,
+    #  marker='o', c='darkgreen', label='PEL coherent tune shift')
     ax.ax_joint.plot(0, 0, marker='o', c='darkgrey', label='Machine tune')
     ax.ax_joint.set_xlabel(r'$\Delta Q_x/\Delta Q_\mathrm{RMS}$')
     ax.ax_joint.set_ylabel(r'$\Delta Q_y/\Delta Q_\mathrm{RMS}$')
@@ -40,18 +40,18 @@ if __name__ == '__main__':
     J = np.column_stack((Jx, Jy)).T
     a = -np.array([[9.20e-05, -6.54e-05], [-6.54e-05, 9.63e-05]])
     Jz = trunc_exp_rv(low=0, high=2.5**2, scale=1.0, size=n_particles)
-    dQmax = 1e-3
-    dQxPEL, dQyPEL = get_pelens_tune(
-        Jz, max_tune_shift_x=dQmax, max_tune_shift_y=dQmax)
+    dQmax = 2.23e9
+    dQxPEL, dQyPEL = get_rfq_tune(Jz,
+                                  v_2=2.23e9)
     dQxOCT, dQyOCT = get_octupole_tune(a, J)
     rms_x, rms_y = np.sqrt(np.var(dQxPEL+dQxOCT)
                            ), np.sqrt(np.var(dQyPEL+dQxOCT))
     ax = plot_spread(dQxPEL+dQxOCT, dQyPEL+dQyOCT,
                      normalise=False, color='#4CB391')
     ax.ax_marg_x.hist(dQxPEL,
-                      color=palette[-1], alpha=0.5, bins=25)
+                      color=palette[-2], alpha=0.5, bins=25)
     ax.ax_marg_y.hist(dQyPEL,
-                      color=palette[-1], alpha=0.5, bins=25, orientation='horizontal')
+                      color=palette[-2], alpha=0.5, bins=25, orientation='horizontal')
     plt.savefig(
-        'Results/'+'PELOCT-'+'.pdf', bbox_inches='tight')
+        'Results/'+'RFQOCT-'+'.pdf', bbox_inches='tight')
     plt.show()
